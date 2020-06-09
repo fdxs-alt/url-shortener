7bbd3bd0a5efd1cc30fcb465bbc54b0145de4a9c
Jakub Sukiennik <kuba1207710@gmail.com>
2020-06-09 16:11:05 +0200

    short URL

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express();
-const dotenv = require('dotenv')
-dotenv.config({ path: "./config.env" });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT)
