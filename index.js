b63277be62d848c88d86926406bf4ac6ca94440c
Jakub Sukiennik <kuba1207710@gmail.com>
2020-06-09 15:14:44 +0200

    commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
-
-const PORT = process.env.port || 5000;
+const dotenv = require('dotenv')
+dotenv.config({ path: "./config.env" });
+const PORT = process.env.PORT || 5000;
 app.listen(PORT)
