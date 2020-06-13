36eb17773a2f6f7e153bc5de4c3136c1c94aa8be
Jakub Sukiennik <kuba1207710@gmail.com>
2020-06-13 12:12:13 +0200

    commit

--- urlshortener/Routes/shortener.js
+++ urlshortener/Routes/shortener.js
@@ -4,18 +4,19 @@ const validURL = require("valid-url");
 const shortID = require("shortid");
 const URL = require("../models/Url");
 const dotenv = require("dotenv");
-const cors = require("cors");
+const cors = require('cors')
 dotenv.config({ path: "./.env.test" });
 
 router.post("/", async (req, res) => {
   const { longUrl } = req.body;
   const baseUrl = process.env.baseUrl;
   if (!validURL.isUri(baseUrl)) return res.status(422).json("Invalid url");
+
   const urlCode = shortID.generate();
   if (!validURL.isUri(longUrl)) return res.status(422).json("Invalid url");
   try {
     let url = await URL.findOne({ longUrl });
-    if (url) res.json(url);
+    if (url) return res.status(200).json(url);
     else {
       const shortUrl = baseUrl + "/url/" + urlCode;
       url = new URL({
@@ -32,15 +33,16 @@ router.post("/", async (req, res) => {
     res.status(500).json("Error");
   }
 });
-router.get("/:code", async (req, res) => {
-  try {
-    const url = await URL.findOne({ urlCode: req.params.code });
-    if (!url) return res.status(400).json("No url");
-    res.redirect(url.longUrl);
-  } catch (error) {
-    console.log(err);
-    res.status(500).json("Server Error");
-  }
-});
+router.get('/:code', async (req, res) => {
+    try {
+        const url = await URL.findOne({urlCode:req.params.code})
+        if(!url)
+        return res.status(400).json("No url")
+        res.redirect(url.longUrl);
+    } catch (error) {
+        console.log(err)
+        res.status(500).json('Server Error')
+    }
+})
 
 module.exports = router;
