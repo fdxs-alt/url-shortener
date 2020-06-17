827337c9640f74ba432efd77b2da742f3ded440f
Jakub Sukiennik <kuba1207710@gmail.com>
2020-06-17 11:05:19 +0200

    message

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const dotenv = require('dotenv')
 dotenv.config({ path: "./env.test" });
 const connectDB = require('./util/db')
 const cors = require('cors')
+const path = require('path')
 app.use(express.json())
 app.use(cors());
 connectDB();
 app.use('/url', require('./Routes/shortener'));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
